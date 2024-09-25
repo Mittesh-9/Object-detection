018df3a856f978ab9f116fcd8adad60ab4c23bd3
Mittesh <mitteshwaghule09@gmail.com>
2024-09-25 16:26:36 +0530

    added file paths but left the weights file because it is too large

--- detect.py
+++ detect.py
@@ -8,10 +8,10 @@ import time
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 
-weightsfile = ''
-classfile = ''
-cfgfile = ''
-sample_img1 = ''
-input_dir = ''
-output_dir = ''
+weightsfile = '/home/mittesh/Downloads/Datasets/yolo_v3_dataset_weights_file/yolov3.weights'
+classfile = 'yolo_v3_kernel_datatset/Input/coco.names'
+cfgfile = 'yolo_v3_kernel_datatset/Input/yolov3.cfg'
+sample_img1 = 'yolo_v3_kernel_datatset/Input/dog.jpg'
+input_dir = 'Object-detection/yolo_v3_kernel_datatset/Input'
+output_dir = 'Object-detection/yolo_v3_kernel_datatset/Output'
 nms_thesh = 0.5
